d678db9bd82b719e9bb3daf71dc9f418ff9d4f15
BCN 2 <BCN 2@DESKTOP-BCN2>
2020-02-28 12:57:33 -0300

    update JavaDoc

--- Electrodomesticos/doc/package-search-index.js
+++ Electrodomesticos/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ejecutable"},{"l":"electrodomestico"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"awakelab.ejecutable"},{"l":"awakelab.electrodomestico"}]
--- Electrodomesticos/doc/type-search-index.js
+++ Electrodomesticos/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"electrodomestico","l":"Electrodomestico"},{"p":"electrodomestico","l":"Lavadora"},{"p":"ejecutable","l":"Main"},{"p":"electrodomestico","l":"Televisor"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"awakelab.electrodomestico","l":"Electrodomestico"},{"p":"awakelab.electrodomestico","l":"Lavadora"},{"p":"awakelab.ejecutable","l":"Main"},{"p":"awakelab.electrodomestico","l":"Televisor"}]
